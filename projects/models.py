b769be635002e79581dd84cf2cf4f51227f47b82
My Nguyen <mykieudonguyen@gmail.com>
2023-06-05 14:33:11 -0500

    Feature 3 complete

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+# Model to store user projects
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="projects",
+        on_delete=models.CASCADE,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
