53fa492a592ba5af459f44fcc08e706998c55fb4
My Nguyen <mykieudonguyen@gmail.com>
2023-06-05 17:21:40 -0500

    Feature 15 complete

---
+++ tasks/forms.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from tasks.models import Task
+
+# Form to create new Task
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = (
+            "name",
+            "start_date",
+            "due_date",
+            "project",
+            "assignee"
+        )
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from tasks.forms import TaskForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+# View to Create new task
+
+
+@login_required
+def create_task(request):
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list_projects")
+
+    else:
+        form = TaskForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "tasks/create_task.html", context)
